feb47a815947c1ccdd5ffa1ced82207916258eee
Erik Garces Matos <erik_senac@hotmail.com>
2018-12-17 09:04:11 -0200

    tate

--- public/js/src/index2.js
+++ public/js/src/index2.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 async function minhafuncao(parametro) {
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
         const page = await browser.newPage();
       
         await page.goto('https://developers.google.com/web/');
