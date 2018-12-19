cf94ab98f43857965c27a5f3a7ff3c9d33927264
Erik Garces Matos <erik_senac@hotmail.com>
2018-12-19 08:55:48 -0200

    teteteet

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-setInterval(() => {
+
   express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+ 
   .get('/teste', async (req, res) => {
     var funcao =  require('./public/js/src/index2');
     var executafuncao = await funcao(55);
     res.send({teste:executafuncao,dois:2});
   })
+  .get('/', (req, res) => res.render('pages/index'))
   .get('/google/:id', async (req, res) => {
     var parametro = req.params.id;
     var funcao =  require('./public/js/src/index3');
@@ -24,6 +25,9 @@ setInterval(() => {
     var oteste = funcao(req.params.id, req.params.hash);
     res.send(oteste);
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-}, 30000);
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  setInterval(() => {
+    express().get('/', (req, res) => res.send('pages/index'))
+  }, 30000);
+
 
